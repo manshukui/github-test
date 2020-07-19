c9be4351aba6bde105ed601db87e0a5a069b4233
mansk <2677498118@qq.com>
2020-07-19 17:36:54 +0800

    first commit

---
+++ src/test/java/com/baizhi/test/GithubTests.java
@@ -0,0 +1,17 @@
+package com.baizhi.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithubTests {
+
+    @Test
+    public void test() {
+        
+    }
+
+}
