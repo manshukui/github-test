e2e8d811e7065628f098bc6ef19ba3b5fcb7170e
mansk121 <2677498118@qq.com>
2020-07-19 18:46:43 +0800

    第二版提交

--- src/test/java/com/baizhi/test/GithubTests.java
+++ src/test/java/com/baizhi/test/GithubTests.java
@@ -11,7 +11,13 @@ public class GithubTests {
 
     @Test
     public void test() {
-        
+        System.out.println("你好，请多指教");
+    }
+
+    @Test
+    public void test2() {
+        int a = 10;
+        System.out.println(a+10);
     }
 
 }
